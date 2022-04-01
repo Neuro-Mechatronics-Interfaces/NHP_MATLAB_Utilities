44f756eda54ef8f089f8871b2ced179b176b4555
Max-MI115 <max.murphy11@gmail.com>
2022-04-01 16:32:30 -0400

    Add parameters function.

---
+++ parameters.m
@@ -0,0 +1,57 @@
+function varargout = parameters(varargin)
+%PARAMETERS Return parameters struct, which sets default values for things like epoch durations etc.
+%
+% Example 1
+%   pars = utils.parameters(); % Returns full "default parameters" struct.
+%
+% Example 2
+%   f = utils.parameters('raw_data_folder'); % Returns raw data folder value
+%
+% I tried to pull the key parameters, which are really path information
+% from your local mapping to where the raw data is and where any
+% auto-exported figures/data objects should go. Both
+% `generated_data_folder` and `raw_data_folder` should be the folder that
+% contains "animal name" folders (e.g. the folder with `Forrest` in it). 
+%
+% See also: Contents, load_tmsi_raw
+
+pars = struct;
+
+% % % Trying to pull the "relevant" ones to the top ... % % % 
+pars.generated_data_folder = 'R:\NMLShare\generated_data\primate\DARPA_N3\N3_Patch';
+pars.preliminary_output_folder = fullfile(pars.generated_data_folder, '.preliminary'); % . prefix defaults to "Hidden" on Windows
+pars.raw_data_folder = 'R:\NMLShare\raw_data\primate';
+pars.default_training_log_file = 'G:\Shared drives\NML_NHP\Monkey Training Records\Training.xlsx';
+pars.default_metadata_file = 'R:\NMLShare\raw_data\primate\Forrest\metadata.xlsx';
+pars.version = 1.0; % "Version" of utils/parameter repo code
+
+N = numel(varargin);
+if nargout == 1
+    if rem(N, 2) == 1
+        varargout = {pars.(varargin{end})};
+        return;
+    else
+        f = fieldnames(pars);
+        for iV = 1:2:N
+            idx = strcmpi(f, varargin{iV});
+            if sum(idx) == 1
+               pars.(f{idx}) = varargin{iV+1}; 
+            end
+        end
+        varargout = {pars};
+        return;
+    end
+else
+    f = fieldnames(pars);
+    varargout = cell(1, nargout);
+    for iV = 1:numel(varargout)
+        idx = strcmpi(f, varargin{iV});
+        if sum(idx) == 1
+            varargout{iV} = pars.(f{idx}); 
+        else
+            error('Could not find parameter: %s', varargin{iV}); 
+        end
+    end
+end
+
+end
