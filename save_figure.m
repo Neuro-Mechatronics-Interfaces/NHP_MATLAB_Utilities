80fcce99e6f49953d554aaafd6b89b1cbd604f86
Max-MI115 <max.murphy11@gmail.com>
2023-09-28 17:18:40 -0400

    Adds "AddAnnotation" option to save_figure

--- save_figure.m
+++ save_figure.m
@@ -11,6 +11,7 @@ function save_figure(fig, output_folder, name_stem, options)
 %                   related to different export types.
 %  
 % Options:
+%     'AddAnnotation' (1,1) logical = true - Set to false to prevent annotation parsing from output folder name.
 %     'CloseFigure' (1,1) logical = true - Set to false to prevent auto-delete of figure handle after successful saves.
 %     'ExportAs' cell = {'.png'} - Can be an arbitrary cell array of '.*' extensions for valid filetypes. See also: saveas
 %     'Tag' {mustBeTextScalar} = '' - Can be used to append some differentiating filename tag for multiple iterations of similar file saved in same sub-folder.
@@ -27,6 +28,7 @@ arguments
     fig (1,1) matlab.ui.Figure
     output_folder {mustBeTextScalar}
     name_stem {mustBeTextScalar}
+    options.AddAnnotation (1,1) logical = true;
     options.CloseFigure (1,1) logical = true;
     options.ExportAs cell = {'.png'}; 
     options.Tag {mustBeTextScalar} = ''
@@ -36,6 +38,21 @@ end
 
 if exist(output_folder,'dir')==0
     mkdir(fullfile(output_folder, options.SubFolders{:}));
+end
+
+if options.AddAnnotation
+    pinfo = strsplit(output_folder,filesep);
+    if startsWith(pinfo{end},'v') && ~isnan(str2double(strrep(pinfo{end},'v','')))
+        v_num = pinfo{end};
+        c_name = pinfo{end-1};
+        c_date = pinfo{end-2};
+        annotation(fig,'textbox',[0.01 0.01 .5 .04],'String',sprintf('%s.m',strrep(c_name,'_','\_')), ...
+            'FitBoxToText','off','EdgeColor','none','Margin',0,...
+            'FontName','Tahoma','HorizontalAlignment','left');
+        annotation(fig,'textbox',[.49 0.95 .5 .04],'String',sprintf('%s: %s',c_date,v_num), ...
+            'FitBoxToText','off','EdgeColor','none','Margin',0,...
+            'FontName','Tahoma','HorizontalAlignment','right');
+    end
 end
 
 if strlength(options.Tag) > 0
