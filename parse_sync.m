16568fc1497fbaaf4652d28debdbbf137982d05a
Max (Lenovo) <max.murphy11@gmail.com>
2024-05-20 19:16:59 -0400

    Adds MBoxtest and parse_sync
    MBoxtest is from File Exchange but modified by Max

---
+++ parse_sync.m
@@ -0,0 +1,19 @@
+function rising = parse_sync(triggers, bit, options)
+%PARSE_SYNC  Parses sync from TMSi TRIGGERS
+arguments
+    triggers (1,:) double
+    bit (1,1) {mustBeInteger}
+    options.InvertLogic (1,1) logical = true;
+    options.DebounceSamples (1,1) double {mustBePositive, mustBeInteger} = 1;
+end
+if options.InvertLogic
+    HIGH = find(bitand(triggers, 2^bit)==0);
+else
+    HIGH = find(bitand(triggers, 2^bit)==2^bit);
+end
+if ~isempty(HIGH)
+    rising = HIGH([HIGH(1) > 1, diff(HIGH) > options.DebounceSamples]);
+else
+    rising = [];
+end
+end
