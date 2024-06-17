dc4053a1b67b401d2087cd827e001b822883ac1c
Max (Lenovo) <max.murphy11@gmail.com>
2024-06-17 10:35:44 -0400

    Adds parsing of falling edges to `parse_sync`

--- parse_sync.m
+++ parse_sync.m
@@ -1,4 +1,4 @@
-function rising = parse_sync(triggers, bit, options)
+function [rising,falling] = parse_sync(triggers, bit, options)
 %PARSE_SYNC  Parses sync from TMSi TRIGGERS
 arguments
     triggers (1,:) double
@@ -15,5 +15,18 @@ if ~isempty(HIGH)
     rising = HIGH([HIGH(1) > 1, diff(HIGH) > options.DebounceSamples]);
 else
     rising = [];
+end
+if nargout < 2
+    return;
+end
+if options.InvertLogic
+    LOW = find(bitand(triggers, 2^bit)==2^bit);
+else
+    LOW = find(bitand(triggers, 2^bit)==0);
+end
+if ~isempty(LOW)
+    falling = LOW([LOW(1) > 1, diff(LOW) > options.DebounceSamples]);
+else
+    falling = [];
 end
 end
